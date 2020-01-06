a9aaac0c51433ed1b0eaed7235e100bd1adcb67c
Justin A <jarata@gmail.com>
2020-01-06 09:27:01 -0800

    added navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <div>
+        <Link to='/'>Welcome Page</Link>
+      </div>
+      <div>
+        <Link to='/characters'>Characters List</Link>
+      </div>
     </header>
   );
 }
