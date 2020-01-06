adfa0a47b64c8c401a42c533bdbe1f1b9e1745bd
Justin A <jarata@gmail.com>
2020-01-06 09:51:06 -0800

    setting response data to state data from axios call testing data to map over

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [rmChars, setRmChars] = useState([]);
+  console.log('rmChars:', rmChars)
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(res => {
+        console.log('res:', res.data);
+        setRmChars(res.data.results)
+      })
+      .catch(err => console.log('error:', err))
   }, []);
 
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
+      {rmChars.map(char => {
+        console.log('char created:', char)
+      })}
     </section>
   );
 }
